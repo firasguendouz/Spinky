f2565f1867be1de0ed554177f89258a86df39e49
firasguendouz <firasguendouzz@gmail.com>
2024-02-01 10:14:45 +0100

    init

--- src/pages/AboutUs/AboutUs.tsx
+++ src/pages/AboutUs/AboutUs.tsx
@@ -5,8 +5,6 @@ import './AboutUs.css';
 import { faDiscord, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 import FeaturedNFTsSection from '../Home/FeaturedNFTsSection';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
 import { useLocation } from 'react-router-dom';
 
 export   const AboutUs = () => {
